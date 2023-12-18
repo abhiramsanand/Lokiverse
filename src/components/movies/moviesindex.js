9e4187b2339cdca90eb1229003b57dc86470e0bf
Abhiram S Anand <abhiramcr7003@gmail.com>
2023-12-18 22:47:12 +0530

    updates

--- src/components/movies/moviesindex.js
+++ src/components/movies/moviesindex.js
@@ -1,12 +1,13 @@
 import Movies from "./movies";
 import Upcoming from "./upcoming";
+import './movies.css'
 
 export default function Moviesindex() {
 
     return (
-        <>
+        <div className="movies">
             <Movies />
             <Upcoming />
-        </>
+        </div>
     );
 }
