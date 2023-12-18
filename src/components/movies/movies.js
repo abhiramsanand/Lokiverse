082d10fcba03c067bf01a4ebcc8321884d7e1a1d
Abhiram S Anand <abhiramcr7003@gmail.com>
2023-12-18 21:54:44 +0530

    updates

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -11,10 +11,10 @@ export default function Movies() {
     <div className='movies'>
       <div className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-dark-1003 relative">
         <div className='md:w-4/6'>
-          <div className="mt-4 text-dark-1003">
+          <div className="text-dark-1003">
             <Link to="/" className="absolute top-4 right-4 hover:text-dark-900 p-2 border border-dark-1003 hover:bg-dark-1003 hover:border-transparent transition-all duration-300">Home</Link>
             <h1 className="text-2xl font-bold">The Movies</h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-center gap-5">
               <MoviesCard name="Kaithi" img={b1} />
               <MoviesCard name="Vikram" img={b2} />
               <MoviesCard name="Leo" img={b3} />
--- src/components/movies/upcoming.js
+++ src/components/movies/upcoming.js
@@ -8,12 +8,12 @@ import b4 from '../../assets/vikram2.avif'
 
 export default function Upcoming() {
   return (
-    <div className='movies'>
-      <div className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-dark-1003 relative">
+    <div className='upcoming'>
+      <div className="flex w-full h-screen flex-col md:flex-row items-center justify-center text-dark-1003 relative">
         <div className='md:w-5/6'>
-          <div className="mt-4 text-dark-1003">
+          <div className="text-dark-1003">
           <h1 className="text-2xl font-bold">The Future</h1>
-            <div className="grid grid-cols-1 md:grid-cols-4 justify-center mt-4 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-4 justify-center gap-5">
               <MoviesCard name="Kaithi2" img={b1} />
               <MoviesCard name="Rolex Spin-Off" img={b2} />
               <MoviesCard name="Leo2" img={b3} />
